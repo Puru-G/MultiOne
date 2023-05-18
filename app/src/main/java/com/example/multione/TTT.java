7c8957115c55373358c57da231894d420fc1e29a
Puru-G <211b232@juetguna.in>
2023-05-18 16:46:54 +0530

    flappy game over xml

--- app/src/main/java/com/example/multione/TTT.java
+++ app/src/main/java/com/example/multione/TTT.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -73,7 +72,7 @@ public class TTT extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ttt);
-        ImageButton home = findViewById(R.id.imageButton2);
+        Button home =(Button) findViewById(R.id.Home_Button);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
