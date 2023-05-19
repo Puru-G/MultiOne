6e2ab5fd3d8c90506ba8cae42f492ab18a523535
Puru-G <211b232@juetguna.in>
2023-05-19 20:30:02 +0530

    Start up screen added

---
+++ app/src/main/java/com/example/multione/Start_Screen.java
@@ -0,0 +1,44 @@
+package com.example.multione;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class Start_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent mainIntent = new Intent(Start_Screen.this, MainActivity.class);
+                    startActivity(mainIntent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
